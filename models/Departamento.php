2533a15b2ebfc1f955031cec235067433c093bff
Martin <skydiver@users.noreply.github.com>
2016-10-05 11:53:29 -0300

    * tables * departamento model

---
+++ models/Departamento.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Martin\Departamentos\Models;
+
+    use Model;
+
+    class Departamento extends Model {
+
+        public $table = 'martin_departamentos';
+
+    }
+
+?>
---
+++ updates/create_tables.php
@@ -0,0 +1,35 @@
+<?php
+
+    namespace Martin\Departamentos\Updates;
+
+    use Schema;
+    use October\Rain\Database\Updates\Migration;
+
+    class CreateTables extends Migration {
+
+        public function up() {
+
+            Schema::create('martin_departamentos', function($table) {
+                $table->increments('id')->unsigned();
+                $table->string('name', 80);
+                $table->timestamps();
+            });
+
+            Schema::create('martin_localidades', function($table) {
+                $table->increments('id')->unsigned();
+                $table->integer('departamento_id')->unsigned();
+                $table->foreign('departamento_id')->references('id')->on('martin_departamentos');
+                $table->string('name', 150);
+                $table->timestamps();
+            });
+
+        }
+
+        public function down() {
+            Schema::drop('martin_localidades');
+            Schema::drop('martin_departamentos');
+        }
+
+    }
+
+?>
